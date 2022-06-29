c2d7c22b929d95a44e3e68ddc56828e64aae84b6
asyinka <asyinka@gmail.com>
2022-06-29 19:42:12 +0100

    code to calculate an hypotenuse and a simple increase/reset/decrease code

---
+++ index.js
@@ -0,0 +1,51 @@
+console.log("Hello World")
+// const PI = 3.142;
+// let radius = window.prompt("Enter the radius of the circle");
+// radius = Number(radius);
+// let circumference = 2 * PI * radius;
+// console.log ("The circumferece of the circle is", circumference);
+
+let username;
+username = String(username);
+document.getElementById("myButton").onclick = function(){
+    username = document.getElementById("myText").value;
+    console.log(username);
+    document.getElementById("myLabel").innerHTML = "Welcome, " + username;
+};
+
+let a;
+let b;
+let c;
+
+// a = window.prompt("Enter the value of side a");
+// a = Number(a)
+// b = window.prompt("Enter the value of side b");
+// b = Number(b);
+// c = Math.pow(a, 2) + Math.pow(b, 2);
+// c = Math.sqrt(c);
+// console.log ("The value of the hypotenuse side is", c, "cm")
+
+document.getElementById("submitButton").onclick = function(){
+   a = document.getElementById("aTextBox").value;
+   a = Number(a);
+   b = document.getElementById("bTextBox").value;
+   b = Number(b);
+   c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+   console.log ("The value of c is", c);
+   document.getElementById("cLabel").innerHTML = "Side C is " + c;
+};
+let count = 0;
+
+document.getElementById("increaseButton").onclick = function(){
+    count += 2; 
+    document.getElementById("countLabel").innerHTML = count;
+}
+document.getElementById("resetButton").onclick = function(){
+    count = 0; 
+    document.getElementById("countLabel").innerHTML = count;
+};
+document.getElementById("decreaseButton").onclick = function(){
+    count -= 2; 
+    document.getElementById("countLabel").innerHTML = count;
+};
+
